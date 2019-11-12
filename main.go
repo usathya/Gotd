85dda7228d59dbcc393639b0b0ffc5ad3925ac73
Don Schenck <don.schenck@gmail.com>
2019-11-12 15:42:08 -0500

    Switch to Go

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+    "fmt"
+    "log"
+    "net/http"
+)
+
+func homePage(w http.ResponseWriter, r *http.Request){
+    fmt.Fprintf(w, "The best way out is always through.")
+}
+
+func handleRequests() {
+    http.HandleFunc("/", homePage)
+    log.Fatal(http.ListenAndServe(":10000", nil))
+}
+
+func main() {
+    handleRequests()
+}
