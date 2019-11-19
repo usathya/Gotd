f7bd116b60a9f53359e3eb3b327c737ff680ca17
unknown <dschenck@win.redhat.com>
2019-11-19 10:52:36 -0500

    Converted so it returns random quotes

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # qotd
-Quote of the Day microservice
+## "Quote of the Day" microservice written in Go.
+
+This is used by various tutorials and articles about CodeReady Workspaces 2.0.
+
+For more information, see the Github page.
--- main.go
+++ main.go
@@ -1,20 +1,28 @@
 package main
 
 import (
-    "fmt"
-    "log"
-    "net/http"
+	"fmt"
+	"log"
+	"math/rand"
+	"net/http"
 )
 
-func homePage(w http.ResponseWriter, r *http.Request){
-    fmt.Fprintf(w, "The best way out is always through.")
+func homePage(w http.ResponseWriter, r *http.Request) {
+	var quotes [5]string
+	quotes[0] = "40 is the old age of youth, while 50 is the youth of old age. - Victor Hugo"
+	quotes[1] = "Knowledge is power. – Francis Bacon"
+	quotes[2] = "Life is really simple, but we insist on making it complicated. - Confucius"
+	quotes[3] = "This above all, to thine own self be true. - William Shakespeare"
+	quotes[4] = "Live your dreams. – Les Brown"
+
+	fmt.Fprintf(w, quotes[rand.Intn(5)])
 }
 
 func handleRequests() {
-    http.HandleFunc("/", homePage)
-    log.Fatal(http.ListenAndServe(":10000", nil))
+	http.HandleFunc("/", homePage)
+	log.Fatal(http.ListenAndServe(":10000", nil))
 }
 
 func main() {
-    handleRequests()
+	handleRequests()
 }
